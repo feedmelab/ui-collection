930a11d90e75f9f2ea0b812a03001e09b58c2a83
feedmelab <xtorner@gmail.com>
2023-07-17 14:29:42 +0200

    Hide std cursor

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,8 +1,9 @@
 import { ReactElement } from 'react';
 
 const HomePage: React.FC = (): ReactElement => {
+  const gf_logo = '../../assets/gh.png';
   return (
-    <div className='container '>
+    <div className='container m-5'>
       <h5>Download or clone allcode used in this app from:</h5>
       <a
         href='https://github.com/feedmelab/ui-collection.git'
@@ -10,10 +11,7 @@ const HomePage: React.FC = (): ReactElement => {
         rel='noopener'
         title='Download link for git project'
       >
-        <img
-          src='public/assets/gh.png'
-          alt='Logo that route to  github ui-project'
-        />
+        <img src={gf_logo} alt='Logo that route to  github ui-project' />
       </a>
     </div>
   );
