5bcf90c794ba57d1ed682b52e43485132bdc85d1
feedmelab <xtorner@gmail.com>
2023-07-21 09:02:17 +0200

    Ecosystem Moon y Sun | Add moon texture and a duck and a new hawk

--- src/components/Effects/DrawingBeziers/Pajaro.tsx
+++ src/components/Effects/DrawingBeziers/Pajaro.tsx
@@ -30,6 +30,7 @@ const Pajaro: React.FC<PajaroProps> = ({ position }) => {
         backgroundImage: `url(${image})`,
         backgroundSize: 'cover',
         transition: '0.5s',
+        zIndex: -1,
       }}
     />
   );
